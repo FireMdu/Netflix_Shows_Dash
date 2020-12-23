7c5ed9835f7aa6f8077bd8f48b76bc61b372ea58
Mduduzi Mlilo <539592@Mech.wits.ac.za>
2020-12-23 10:47:00 +0200

    Cleaned up code and handling erros

--- tutorials/matplotlib_streamlit.py
+++ tutorials/matplotlib_streamlit.py
@@ -85,7 +85,9 @@ def select_file():
     chose_data_path = st.checkbox("Custom Data Path")
     database_extensions = ['csv', 'sqlite', 'xlsx', 'xlsm', 'txt']
     if chose_data_path:
-        uploader = st.file_uploader("Upload data file", type=database_extensions)
+        uploader = st.file_uploader("Upload data file",
+                                    type=database_extensions
+                                    )
         data_file = uploader
     else:
         data_folder = os.path.join(os.getcwd(), 'data')
@@ -99,16 +101,17 @@ def select_file():
             pass
 
 
-def get_data():
+def get_data(data_container):
     """Load chosen datafile"""
     global data_file
 
     data = pd.DataFrame({})
     try:
-        data = NetflixFilms(pd.read_csv, data_file)
+        data = data_container(pd.read_csv, data_file)
         data = data.data
-    except Exception:
-        st.text("Oooopsie! Seems like you loaded a wrong file type:\nLet's try a .csv file")
+    except pd.errors.ParserError:
+        st.text("Oooopsie! Seems like you loaded a wrong "
+                "file type:\nLet's try a .csv file")
         try:
             sys.exit(1)
         except SystemExit:
@@ -120,7 +123,7 @@ def raw_data_sidebar_objects():
     """Define objects to be placed by the sidebar"""
     st.sidebar.title("Tools")
     st.sidebar.markdown("### Raw Data")
-    with st.sidebar.beta_expander(" "):
+    with st.sidebar.beta_expander("Data Input"):
         select_file()
 
 
@@ -151,14 +154,19 @@ def data_cleaning_main_page():
                         cleaning_radio_cols, empty_cols[ind],
                         st.radio(f"{empty_cols[ind]}",
                                  ("Drop", "Fill with"),
-                                 key=f"{empty_cols[ind]}")
+                                 key=f"{empty_cols[ind]}"
+                                 )
                     )
-                    if getattr(cleaning_radio_cols, empty_cols[ind]) == "Fill with":
+                    button_opt = getattr(cleaning_radio_cols, empty_cols[ind])
+                    if button_opt == "Fill with":
                         setattr(cleaning_radio_cols,
                                 empty_cols[ind] + '_input',
                                 st.text_input("Sub", key=empty_cols[ind]))
                         empty_cols_cleaner.update(
-                            {empty_cols[ind]: getattr(cleaning_radio_cols, empty_cols[ind] + '_input')})
+                            {empty_cols[ind]: getattr(
+                                cleaning_radio_cols,
+                                empty_cols[ind] + '_input')}
+                        )
                     else:
                         setattr(cleaning_radio_cols,
                                 empty_cols[ind] + '_input',
@@ -173,11 +181,11 @@ def data_cleaning_main_page():
                     with st.spinner("Cleaning ..."):
                         cleaned_data = clean_data(dash_data_bak, **kwargs)
                         status.markdown("Success :white_check_mark:")
-    if cleaned_data.empty:
-        pass
-    else:
-        st.markdown('#### Clean Data')
-        st.dataframe(cleaned_data.head())
+            if cleaned_data.empty:
+                pass
+            else:
+                st.markdown('#### Clean Data')
+                st.dataframe(cleaned_data.head())
 
 
 @st.cache
@@ -197,71 +205,109 @@ def generate_pivot(pivot_func, data, **kwargs):
 
 def plot_matplotlib():
     global cleaned_data
-    permited_plots = ['line', 'bar', 'barh', 'hist', 'box', 'kide', 'density',
+
+    permited_plots = ['plot', 'bar', 'barh', 'hist', 'box', 'kide', 'density',
                       'area', 'pie', 'scatter', 'hexbin']
     st.markdown("## Analysis\n***")
 
     pivot_table_arglist = ['values', 'index', 'columns', 'aggfunc', 'margins']
-    aggregating_functions = ['count', 'sum', 'mean', 'median', 'most_frequent']
+    pivot_arglist = ['values', 'index', 'columns']
+    agg_functions = ['count', 'sum', 'mean', 'median', 'most_frequent']
     pivot_table_state = PivotTableArgsState()
 
-    with st.sidebar.beta_expander("Pivot Table"):
-        # TODO: Surely there must be another way to avoid duplicating entries across columns and index variables
+    with st.sidebar.beta_expander("Plot Vars"):
+        # TODO: Surely there must be another way to avoid duplicating entries
+        #  across columns and index variables
         use_pivot = st.checkbox("Non numeric data")
         for opt in pivot_table_arglist:
-            if opt in ['values', 'index', 'columns']:
-                descript = f"Set {opt}"
-                setattr(pivot_table_state, opt, st.multiselect(descript, cleaned_data.columns, key=opt))
+            if opt in pivot_arglist:
+                label = f"Set {opt}"
+                setattr(
+                    pivot_table_state,
+                    opt,
+                    st.multiselect(
+                        label, cleaned_data.columns, key=opt
+                    )
+                )
             if use_pivot:
                 continue
             elif opt == 'aggfunc':
-                # TODO: Include muliple agg funcs
-                setattr(pivot_table_state, opt, st.selectbox("Agg Func", aggregating_functions, key=opt))
-            elif opt=='margins':
-                setattr(pivot_table_state, opt, st.checkbox("Sub/Totals", value=False))
+                # TODO: Include multiple agg funcs
+                setattr(
+                    pivot_table_state,
+                    opt,
+                    st.selectbox(
+                        "Agg Func", agg_functions, key=opt
+                    )
+                )
+            elif opt == 'margins':
+                setattr(pivot_table_state,
+                        opt,
+                        st.checkbox("Sub/Totals", value=False))
+    if not use_pivot:
+        pivot_table_kwargs = {key: getattr(pivot_table_state, key)
+                              for key in pivot_table_arglist}
+    pivot_kwargs = {key: getattr(pivot_table_state, key)
+                    for key in pivot_arglist}
 
-    with st.beta_expander("Pivot Table"):
-        values = pivot_table_state.values
-        index = pivot_table_state.index
-        columns = pivot_table_state.columns
-        aggfunc = pivot_table_state.aggfunc
-        margins = pivot_table_state.margins
-        if use_pivot:
-            pivot_data = generate_pivot(pd.pivot,
-                                        cleaned_data,
-                                        values=values,
-                                        index=index,
-                                        columns=columns)
-        else:
-            pivot_data = generate_pivot(pd.pivot_table,
-                                        cleaned_data,
-                                        values=values,
-                                        index=index,
-                                        columns=columns,
-                                        aggfunc=aggfunc,
-                                        fill_value=0,
-                                        margins=margins,
-                                        margins_name="Sub/Total(s)")
-        st.dataframe(pivot_data)
+    num_params = sum([len(obj) > 0 for obj in pivot_kwargs.values()])
+    pivot_data = pd.DataFrame({})
+    if num_params >= 2:
+        with st.beta_expander("Pivot Table"):
+            if use_pivot:
+                pivot_data = generate_pivot(pd.pivot,
+                                            cleaned_data,
+                                            **pivot_kwargs)
+            else:
+                pivot_data = generate_pivot(pd.pivot_table, cleaned_data,
+                                            margins_name="Sub/Total(s)",
+                                            fill_value=0, **pivot_table_kwargs)
+            if not pivot_data.empty:
+                st.dataframe(pivot_data)
+            else:
+                pass
 
     plot_selector_tab = st.sidebar.beta_expander("Plot")
-    
+
+    # sidebar for matplotlib tools
     with plot_selector_tab:
+        plot_kwargs = {}
         select_plot_type = st.selectbox("Graph Type", permited_plots)
         color_selector_col, _ = st.beta_columns([0.9, 1])
         with color_selector_col:
             plot_color = st.color_picker("Plot color")
-            # st.markdown("***")
-            aggregate = st.selectbox("Aggregation:", aggregating_functions)
+            x_label = st.text_input("x-axis label:")
+            y_label = st.text_input("y-axis label:")
 
         with _:
-            x_axis_var = st.selectbox("x-axis var", cleaned_data.columns.to_list())
-            y_axis_var = st.selectbox("y-axis var", cleaned_data.columns.to_list())
+            x_axis_var = st.multiselect("x-axis var",
+                                        cleaned_data.columns.to_list()
+                                        )
+            y_axis_var = st.multiselect("y-axis var",
+                                        cleaned_data.columns.to_list()
+                                        )
 
-        grouper_list = st.multiselect('Chose Grouper', cleaned_data.columns.to_list(), default=None)
+        grouper_list = st.multiselect('Chose Grouper',
+                                      cleaned_data.columns.to_list(),
+                                      default=None
+                                      )
+        plot_kwargs.update({'color': plot_color})
         _, calculate_but_space = st.beta_columns([1, 0.3])
         with calculate_but_space:
             calculate_plot_but = st.button("Plot")
+    try:
+        plot_pivot_data(pivot_data.index.get_level_values(x_axis_var[0]), pivot_data[y_axis_var[0]].values, kind=select_plot_type, **plot_kwargs)
+    except IndexError:
+        pass
+
+
+def plot_pivot_data(x, y, kind='plot', **kwargs):
+    # breakpoint()
+    with plt.xkcd():
+        fig, ax = plt.subplots()
+        getattr(plt, kind)(x, y, **kwargs)
+        plt.xticks(rotation=80)
+        st.pyplot(fig)
 
 
 def get_pivot_table(data, **kwargs):
@@ -271,23 +317,33 @@ def get_pivot_table(data, **kwargs):
 def raw_data_main_page():
     """Define objects to be defined in the main page"""
     global dash_data
-    dash_data = get_data()
-    st.dataframe(dash_data.head())
+    dash_data = get_data(ReadExcelDatabase)
+    if not dash_data.empty:
+        st.dataframe(dash_data.head())
 
-    # some statistics
-    st.markdown("### Data Statistics\n***")
-    missing_data_tab, missing_data_plot = st.beta_columns((1, 1.7))
-    new_df = pd.DataFrame(dash_data.isnull().sum(), columns=['Missing count']).reindex()
-    new_df.sort_values('Missing count', ascending=False, inplace=True)
-    with missing_data_tab:
-        st.dataframe(new_df, 250, 400)
+        # some statistics
+        new_df = pd.DataFrame(dash_data.isnull().sum(),
+                              columns=['Missing count']
+                              ).reindex()
+        new_df.sort_values('Missing count', ascending=False, inplace=True)
+        # breakpoint()
+        if new_df['Missing count'].any():
+            st.markdown("### Data Statistics\n***")
+            missing_data_tab, missing_data_plot = st.beta_columns((1, 1.7))
+            with missing_data_tab:
+                st.dataframe(new_df)
 
-    with missing_data_plot:
-        with plt.xkcd():
-            fig, ax = plt.subplots()
-            plt.bar(new_df.index.values, new_df['Missing count'])
-            plt.xticks(rotation=80)
-            st.pyplot(fig)
+            with missing_data_plot:
+                with plt.xkcd():
+                    fig, ax = plt.subplots()
+                    plt.bar(new_df.index.values, new_df['Missing count'])
+                    plt.xticks(rotation=80)
+                    st.pyplot(fig)
+        else:
+            st.text("Yaaaay, you don't have missing data!")
+            st.balloons() # I think these are annoying
+    else:
+        pass
 
 
 def streamlit_main():
@@ -300,10 +356,5 @@ def streamlit_main():
     plot_matplotlib()
 
 
-def read_data():
-    """Read the data from the uploader button"""
-    pass
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     streamlit_main()
